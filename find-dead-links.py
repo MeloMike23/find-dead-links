4cb593c22a3f6f0f79e3895ebadab0efdfd805ce
MeloMike23 <michael.melohn23@gmail.com>
2021-03-21 23:46:00 -0500

    Updateed find-dead-links python file

--- find-dead-links.py
+++ find-dead-links.py
@@ -1,8 +1,68 @@
-from sys import argv
-from pathlib import Path
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from sys import argv
 
-progname = Path(argv[0]).stem
+def fdl(url, depth):
+	#List of all links
+	aLinks = []
+
+	#List of good links
+	gLinks = []
+
+	#Checks to make sure url is good
+	try:
+		r = requests.get(url)
+		if r.status_code not in [200, 302]:
+			print('Bad Link')
+	except:
+		print(url, ' = Bad Link')
+	
+	soup = BeautifulSoup(r.text, "html.praser")
+
+	#Looking for links in URL
+	for i in soup.find_all("a"):
+		link = i.get("href")
+		if link.startswith("/"):
+			if url.endswith("/"):
+				link = url[:-1] + link
+			else:
+				link = url + link
+		aLinks.append(link)
+	
+	#Looks for and prints bad links
+	for link in aLinks:
+		try:
+			r = requests.get(link)
+			#If link is bad print
+			if r.status_code not in [200,302]:
+				print(link, "is a bad link.")
+			#Good links
+			else:
+				gLinks.append(link)
+		except:
+			print(link, " is a bad link.")
+	
+	#Checks depth and calls fdl(url, depth) on good links
+	if depth > 0:
+		for link in gLinks:
+		fdl(link, depth-1)
 
 
+if __name__ == "__main__":
+	if len(argv) == 2:
+		#Find dead links with no depth
+		url = argv[1]
+		fdl(url, 0)
+	elif len(argv) == 3:
+		#Find dead links with depth restricitons
+		url = argv[2]
+		depth_url = argv[1]
+		if depth_url.statswith("-"):
+			try:
+				depth_url = int(depth_url[1:])
+			except:
+				print("ERROR: DEPTH INVALID")
+				exit(1)
+		fdl(url, depth)
+	else:
+		print("ERROR: find-dead-links [-<depth] <URL>")	
